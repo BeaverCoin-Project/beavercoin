74df77bab533c4d9b97a673c0fb3b8e905745c27
NodeCode <info@nodecode.de>
2014-08-01 14:39:21 +0200

    DNS seed

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-//    {"beavercoin.0url.de", "dnsseed.0url.de"},
+    {"beavercoin.org", "dnsseed.beavercoin.org"},
     {NULL, NULL}
 };
 
