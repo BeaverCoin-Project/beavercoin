2dd0fd5d54e8fd78a1b53bae13c06a05d02d7e88
NodeCode <info@nodecode.de>
2014-08-04 11:08:51 +0200

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"beavercoin.org", "dnsseed.beavercoin.org"},
+//    {"beavercoin.org", "dnsseed.beavercoin.org"},
     {NULL, NULL}
 };
 
